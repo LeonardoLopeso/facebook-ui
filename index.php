1f868c8341da3a7e1d065268678d9e12bebf33ff
Leonardo Lopes <l.lopes355@gmail.com>
2021-04-13 20:11:18 -0300

    Reprodução da UI do Facebook

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Login Facebook</title>
 	<!--Aplicar depois as meta tags-->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="css/style.css">
 
